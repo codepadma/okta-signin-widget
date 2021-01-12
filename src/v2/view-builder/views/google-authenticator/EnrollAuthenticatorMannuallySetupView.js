a388724e64039696b62182824cbde33ebf80dc84
tian.gan <tian.gan@okta.com>
2021-01-12 10:30:18 -0800

    feat: draft of google authenticator enroll [WIP-test needed]

---
+++ src/v2/view-builder/views/google-authenticator/EnrollAuthenticatorMannuallySetupView.js
@@ -0,0 +1,15 @@
+import { View } from 'okta';
+import hbs from 'handlebars-inline-precompile';
+
+export default View.extend({
+  className: 'oie-enroll-google-authenticator-manual-setup',
+  template: hbs`
+      <div class="google-authenticator-setup-info-title">{{i18n code="oie.enroll.google_authenticator.cannotScanBarcode.title" bundle="login"}}</div>
+      <p class="google-authenticator-setup-info">
+          {{i18n code="oie.enroll.google_authenticator.manualSetupInstructions" bundle="login"}}
+      </p>
+    `,
+});
+
+
+
